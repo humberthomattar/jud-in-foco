b07e4b04475e3996279d99edbd313d71173241a6
Humbertho Mattar <humberthomattar@gmail.com>
2019-11-14 18:54:31 -0300

    included new users

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('accounts/', views.users_list, name='users'),
+    path('accounts/new/', views.user_new, name='user_new'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,25 @@
+from .forms import UserForm
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from django.contrib import messages
 
 def users_list(request):
     users = User.objects.all()
     return render(request, 'accounts/users.html', {'users': users})
+
+def user_new(request):
+    try:
+        if request.method == "POST":
+            user = UserForm(request.POST)
+            if user.is_valid():
+                user = user.save(commit=False)
+                user.password = make_password(user.password)
+                user.save()
+                messages.success(request, 'Usuário cadastrado com sucesso.')
+            else:
+                messages.warning(request, 'Falha no cadastro')        
+    except Exception as e:
+        messages.warning(request, 'Falha no cadastro. Detalhe: +' + str(e))
+    form = UserForm()
+    return render(request, 'accounts/user_new.html', {'form': form})
