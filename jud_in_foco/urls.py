8230c51d7c9a042ca2698d411bec458e389d954c
Humbertho Mattar <humberthomattar@gmail.com>
2019-11-23 01:13:02 -0300

    Melhoria do dashboard e criaçao do módulo teen

--- jud_in_foco/urls.py
+++ jud_in_foco/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('teens.urls')),
     path('', include('accounts.urls')),
     path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('django.contrib.auth.urls'), name='login'),
